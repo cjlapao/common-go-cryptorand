c20781d335b7137b00b29b19e5ca71b5672c763d
Carlos Lapao <cjlapao@gmail.com>
2024-01-15 09:32:58 +0000

    further functionality added

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # common-go-cryptorand
 
 Library to generate crypto random strings or numbers that are more random than the usual math/rand  
-this will be good to use in security giving an more unpredictable randomness  
+this will be good to use in security giving an more unpredictable randomness.
+
+While it uses the standard math/rand it will use the crypto/rand to generate the seed for the math/rand
+this will make it as the same as using crypto/rand but with the easy of use of math/rand
+
+If you want to use the crypto/rand directly you can use the following functions:
+
+```go
+GetAlphaNumericRandomString()
+GetAlphaRandomString()
+GetNumericRandomString()
+GetRandomString()
+```
+
+Those will directly use the crypto/rand to generate the random string while the `CryptoRand` will use the math/rand
+with the crypto/rand as seed.
--- main.go
+++ main.go
@@ -18,3 +18,48 @@ func Rand() *rand.Rand {
 	generator := rand.New(src)
 	return generator
 }
+
+func (c *CryptoRand) GetRandomNumber(min, max int) (int, error) {
+	return GetRandomNumber(min, max)
+}
+
+func (c *CryptoRand) GetAlphaNumericRandomString(size int) (string, error) {
+	source := AlphaNumericCharacters()
+
+	return c.getRandomFromArray(size, source)
+}
+
+func (c *CryptoRand) GetRandomString(size int) (string, error) {
+	source := AlphaCharacters()
+
+	return c.getRandomFromArray(size, source)
+}
+
+func (c *CryptoRand) GetUpperCaseRandomString(size int) (string, error) {
+	source := make([]string, 0)
+	source = append(source, UpperCaseAlphaCharacters()...)
+
+	return c.getRandomFromArray(size, source)
+}
+
+func (c *CryptoRand) GetLowerCaseRandomString(size int) (string, error) {
+	source := make([]string, 0)
+	source = append(source, LowerCaseAlphaCharacters()...)
+
+	return c.getRandomFromArray(size, source)
+}
+
+func (c *CryptoRand) getRandomFromArray(size int, source []string) (string, error) {
+	result := ""
+	if len(source) > 0 {
+		random := Rand()
+		if size > 0 {
+			for i := 0; i < size; i++ {
+				idx := random.Intn(len(source))
+				result += source[idx]
+			}
+		}
+	}
+
+	return result, nil
+}
