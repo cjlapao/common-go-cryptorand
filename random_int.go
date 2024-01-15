e189277722a589b76927db96436732c9472160c3
Carlos Lapao <cjlapao@gmail.com>
2024-01-15 09:13:10 +0000

    making the helpers using the cryptorand directly instead of the secure source

--- random_int.go
+++ random_int.go
@@ -5,14 +5,13 @@ import (
 	"math/big"
 )
 
-func GetRandomNumber(min, max int) int {
+func GetRandomNumber(min, max int) (int, error) {
 	bg := big.NewInt(int64(max) - int64(min))
 
 	n, err := rand.Int(rand.Reader, bg)
-
 	if err != nil {
-		return 0
+		return 0, err
 	}
 
-	return int(n.Int64() + int64(min))
+	return int(n.Int64() + int64(min)), nil
 }
